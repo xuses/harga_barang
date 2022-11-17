776949523ec3c40d9e5bd235aafe1053632ca395
xuses <33555129+xuses@users.noreply.github.com>
2022-11-17 13:56:49 +0700

    Update ds_project.py

--- ds_project.py
+++ ds_project.py
@@ -9,24 +9,24 @@ from sklearn.metrics import mean_squared_error, r2_score
 import time
 
 #import the data
-data = pd.read_csv("DataClean.csv")
+data = pd.read_csv("gaikindo.csv")
 image = Image.open("house.png")
 st.title("Selamat Datang di Aplikasi cekhargarumah")
 st.image(image, use_column_width=True)
 
 #checking the data
-st.write("Aplikasi ini dibuat untuk mengecek harga rumah menggunakan algoritma kecerdasan buatan. Let's try and see!")
+st.write("Aplikasi ini dibuat untuk mengecek harga mobil menggunakan algoritma kecerdasan buatan. Let's try and see!")
 check_data = st.checkbox("Lihat contoh data")
 if check_data:
     st.write(data.head(10))
 	
-st.write("Mari kita lihat berapa harga rumahnya.")
+st.write("Mari kita lihat berapa harga mobilnya.")
 
 #input the numbers
-sqft_liv = st.slider("Berapa luas ruang tengah yang anda cari?",int(data.sqft_living.min()),int(data.sqft_living.max()),int(data.sqft_living.mean()) )
-bath     = st.slider("Berapa banyak kamar mandi yang dibutuhkan?",int(data.bathrooms.min()),int(data.bathrooms.max()),int(data.bathrooms.mean()) )
-bed      = st.slider("Berapa banyak kamar tidur yang dibutuhkan?",int(data.bedrooms.min()),int(data.bedrooms.max()),int(data.bedrooms.mean()) )
-floor    = st.slider("Berapa tingkat rumah yang anda inginkan?",int(data.floors.min()),int(data.floors.max()),int(data.floors.mean()) )
+sqft_liv = st.slider("Berapa kapasitas mesin yang anda cari?",int(data.sqft_living.min()),int(data.sqft_living.max()),int(data.sqft_living.mean()) )
+bath     = st.slider("Berapa banyak tempat duduk dibutuhkan?",int(data.bathrooms.min()),int(data.bathrooms.max()),int(data.bathrooms.mean()) )
+bed      = st.slider("Berapa banyak jumlah pintu yang dibutuhkan?",int(data.bedrooms.min()),int(data.bedrooms.max()),int(data.bedrooms.mean()) )
+floor    = st.slider("berapa tenaga mesin yang anda inginkan?",int(data.floors.min()),int(data.floors.max()),int(data.floors.mean()) )
 
 #splitting your data
 X = data.drop('price', axis = 1)
@@ -43,6 +43,6 @@ errors = np.sqrt(mean_squared_error(y_test,model.predict(X_test)))
 predictions = model.predict([[sqft_liv,bath,bed,floor]])[0]
 
 #checking prediction house price
-if st.button("Cek Harga Rumah?"):
-    st.header("Harga rumah berdasarkan kebutuhan anda adalah USD {}".format(int(predictions)))
-    st.subheader("Range perkiraan harga rumah anda adalah USD {} - USD {}".format(int(predictions-errors),int(predictions+errors) ))
+if st.button("Cek Harga Mobil?"):
+    st.header("Harga mobil berdasarkan kebutuhan anda adalah Rp {}".format(int(predictions)))
+    st.subheader("Range perkiraan harga mobil anda adalah Rp {} - USD {}".format(int(predictions-errors),int(predictions+errors) ))
