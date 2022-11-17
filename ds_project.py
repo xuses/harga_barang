f38d9b1b5ad3f419b38bcd5876e876e95ec5af50
xuses <33555129+xuses@users.noreply.github.com>
2022-11-17 15:12:46 +0700

    Update ds_project.py

--- ds_project.py
+++ ds_project.py
@@ -9,7 +9,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 import time
 
 #import the data
-data = pd.read_csv("DataClean.csv")
+data = pd.read_csv("gaikindo.csv")
 image = Image.open("house.png")
 st.title("Selamat Datang di Aplikasi cekhargamobil")
 st.image(image, use_column_width=True)
@@ -23,14 +23,14 @@ if check_data:
 st.write("Mari kita lihat berapa harga mobilnya.")
 
 #input the numbers
-sqft_liv = st.slider("Berapa kapasitas mesin yang anda cari?",int(data.sqft_living.min()),int(data.sqft_living.max()),int(data.sqft_living.mean()) )
-bath     = st.slider("Berapa banyak tempat duduk dibutuhkan?",int(data.bathrooms.min()),int(data.bathrooms.max()),int(data.bathrooms.mean()) )
-bed      = st.slider("Berapa banyak jumlah pintu yang dibutuhkan?",int(data.bedrooms.min()),int(data.bedrooms.max()),int(data.bedrooms.mean()) )
-floor    = st.slider("berapa tenaga mesin yang anda inginkan?",int(data.floors.min()),int(data.floors.max()),int(data.floors.mean()) )
+cc = st.slider("Berapa kapasitas mesin yang anda cari?",int(data.CC.min()),int(data.CC.max()),int(data.CC.mean()) )
+seat     = st.slider("Berapa banyak tempat duduk dibutuhkan?",int(data.SEATER.min()),int(data.SEATER.max()),int(data.SEATER.mean()) )
+door      = st.slider("Berapa banyak jumlah pintu yang dibutuhkan?",int(data.DOOR.min()),int(data.DOOR.max()),int(data.DOOR.mean()) )
+ps    = st.slider("berapa tenaga mesin yang anda inginkan?",int(data.HP.min()),int(data.HP.max()),int(data.HP.mean()) )
 
 #splitting your data
-X = data.drop('price', axis = 1)
-y = data['price']
+X = data.drop('BRAND', axis = 1)
+y = data['BRAND']
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=.2, random_state=45)
 
 #modelling step
@@ -40,9 +40,9 @@ model=LinearRegression()
 model.fit(X_train, y_train)
 model.predict(X_test)
 errors = np.sqrt(mean_squared_error(y_test,model.predict(X_test)))
-predictions = model.predict([[sqft_liv,bath,bed,floor]])[0]
+predictions = model.predict([[cc,seat,door,ps]])[0]
 
 #checking prediction house price
 if st.button("Cek Harga Mobil?"):
     st.header("Harga mobil berdasarkan kebutuhan anda adalah Rp {}".format(int(predictions)))
-    st.subheader("Range perkiraan harga mobil anda adalah Rp {} - USD {}".format(int(predictions-errors),int(predictions+errors) ))
+    st.subheader("Range perkiraan harga mobil anda adalah Rp {} - Rp {}".format(int(predictions-errors),int(predictions+errors) ))
